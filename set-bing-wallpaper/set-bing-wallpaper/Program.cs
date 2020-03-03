eda18422183932ec3a550ee3f0038fa911c65c41
Jeroen Maes <jeroen.maes@live.cmo>
2020-03-03 20:57:48 +0100

    catch...

--- set-bing-wallpaper/set-bing-wallpaper/Program.cs
+++ set-bing-wallpaper/set-bing-wallpaper/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace set_bing_wallpaper
 {
@@ -6,18 +7,25 @@ namespace set_bing_wallpaper
     {
         static async Task Main()
         {
-            //Get Bing image of the day
-            var bingImage = new BingImage();
-            var image = await bingImage.GetImage();
+            try
+            {
+                //Get Bing image of the day
+                var bingImage = new BingImage();
+                var image = await bingImage.GetImage();
 
-            //Save image            
-            var imagesFolder = @"c:\_BingImages\";
-            var imageHelper = new ImageHelper();
-            var pathToImage = await imageHelper.DownloadImageToFolder(image.URL, imagesFolder, image.Hash + ".jpg");
-            
-            //Set image as wallpaper
-            var ws = new WallpaperSetter();
-            ws.Set(pathToImage);            
+                //Save image            
+                var imagesFolder = @"c:\_BingImages\";
+                var imageHelper = new ImageHelper();
+                var pathToImage = await imageHelper.DownloadImageToFolder(image.URL, imagesFolder, image.Hash + ".jpg");
+
+                //Set image as wallpaper
+                var ws = new WallpaperSetter();
+                ws.Set(pathToImage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
         }
     }
 }
